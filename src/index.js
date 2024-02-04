d310c74a7a028bd547230df70d44d78301d77f85
Felipe <fefelbf@gmail.com>
2024-02-04 20:27:26 -0300

    comit

--- src/index.js
+++ src/index.js
@@ -25,3 +25,38 @@ changeThemeBtn.addEventListener("change", function(){
         localStorage.setItem("dark", 1);
     }
 });*/
+
+const slider = document.querySelectorAll(".slider")
+const btnPrev = document.getElementById("prev-button")
+const btnNext = document.getElementById("next-button")
+
+let currentSlide = 0;
+function hideSlider(){
+    slider.forEach(item => item.classList.remove("slider1"))
+}
+function showSlider(){
+    slider[currentSlide].classList.add("slider1")
+}
+
+function nextSlider(){
+    hideSlider()
+    if(currentSlide === slider,length -1){
+       currentSlide =0
+    } else{
+        currentSlide++
+    }
+    showSlider()
+}
+
+function prevSlider(){
+    hideSlider()
+    if(currentSlide ===0){
+       currentSlide = slider.length -1
+    } else{
+        currentSlide--
+    }
+    showSlider()
+}
+btnNext.addEventListener("click", nextSlider)
+btnNext.addEventListener("click", prevSlider)
+console.log(slider)
