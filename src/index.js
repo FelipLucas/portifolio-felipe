e43148753604d0011639e13bd69c5c1af183a6ec
Felipe <fefelbf@gmail.com>
2024-02-29 23:28:58 -0300

    comit

--- src/index.js
+++ src/index.js
@@ -9,23 +9,6 @@ function toggleDarkMode(){
     document.body.classList.toggle("dark");
 };
 
-/*//load and save theme/
-function loadTheme() {
-    const darkMode = localStorage.getItem("dark");
-    if(darkMode) {
-        toggleDarkMode();
-    }
-}
-
-loadTheme();
-changeThemeBtn.addEventListener("change", function(){
-    toggleDarkMode();
-    localStorage.removeItem("dark");
-    if(document.body.classList.contains("dark")) {
-        localStorage.setItem("dark", 1);
-    }
-});*/
-
 menu-mobile
 const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar-mobile');
@@ -35,4 +18,16 @@ button.addEventListener('click', function() {
     mobileNavbar.classList.toggle('active');
 });
 
-/*Rolagem de tela*/
+/*carrossel*/
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEL: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    keyboard: true,
+})
